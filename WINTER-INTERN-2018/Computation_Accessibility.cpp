94ee85ee9ddb1fed369ffcd412789959af964cdf
prathiba0311 <47466544+prathiba0311@users.noreply.github.com>
2019-02-22 11:24:43 +0530

    Update Computation_Accessibility.cpp

--- WINTER-INTERN-2018/Computation_Accessibility.cpp
+++ WINTER-INTERN-2018/Computation_Accessibility.cpp
@@ -15,7 +15,7 @@ typedef pair<ll,ll> pll;
 ll maxi=LLONG_MIN;
 ll mini=LLONG_MAX;
 void fast() { ios_base::sync_with_stdio(false); cin.tie(NULL); }
-ll t,i,j,k,n,temp,flag,ans,m,cnt,name;		//i, j and k are loop variables; cnt is count of new number of vertices after collapsing
+ll t,i,j,k,n,temp,flag,ans,m,cnt,name;		//i, j and k are loop variables; cnt is count of new number of vertices after coalesing the nodes
 vl com;						// name is used to assign the values of array L and R
 vl visit;					// com contains the mapping between the original and the new DAG generated
 vl l;						
